c4b0e04be9fe02c2b3ef39bbd0907ebe14dbb1d4
Jesulayomy <jesulayomi081@gmail.com>
2023-01-03 05:28:49 -0800

    5&6

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 for num in range(0, 100):
-    print('{0:2}'.format(num), end="")
     if num != 99:
-        print(', ', end="")
+        print('{0:02}, '.format(num), end="")
     else:
-        print("")
+        print('{0:02}'.format(num))
