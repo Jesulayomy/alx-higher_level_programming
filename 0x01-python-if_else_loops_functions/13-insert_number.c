6981f5a1ada0419ad4c4a38210282ee3bf23bdfa
Jesulayomy <jesulayomi081@gmail.com>
2023-01-03 21:02:19 -0800

    done

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * insert_node - inserts a node into a sorted array
+ * @head: the linked list to modify
+ * @number: to insert into the appropriate location in the list
+ *
+ * Return: the lionked list with the added node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	return (head)
+}
