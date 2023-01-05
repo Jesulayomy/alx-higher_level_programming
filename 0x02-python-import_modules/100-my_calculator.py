2f0145bfd51d261ce00d471134f8e5d12a12aca5
Jesulayomy <jesulayomi081@gmail.com>
2023-01-05 12:04:57 -0800

    last touches

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-if __name__ = "__main__":
+if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
-    if len(argv) != 4:
-        print('Usage: ./{} <a> <operator> <b>'.format(argv[0]))
-        exit (1)
+
+    argc = len(argv)
+    if argc != 4:
+        print('Usage: {} <a> <operator> <b>'.format(argv[0]))
+        exit(1)
+
     a = int(argv[1])
     b = int(argv[3])
     op = argv[2]
@@ -18,4 +21,4 @@ if __name__ = "__main__":
         print('{} * {} = {}'.format(a, b, mul(a, b)))
     else:
         print('Unknown operator. Available operators: +, -, * and /')
-        exit (1)
+        exit(1)
