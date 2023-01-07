0dabf1aa8be7c074fb9cbcac0802608d51794ae5
Jesulayomy <jesulayomi081@gmail.com>
2023-01-07 07:09:07 -0800

    why

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     new_list = []
     for integer in my_list:
-        if integer % 2 == 0
+        if integer % 2 == 0:
             new_list += [True]
         else:
             new_list += [False]
