3bf6b14f5a293f046307c21f2c2794565d2ef3ba
Jesulayomy <jesulayomi081@gmail.com>
2023-01-11 13:24:52 -0800

    fixed ss in items

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     count = 0
-    for key, val in a_dictionary.itemss():
+    for key, val in a_dictionary.items():
         count += 1
     return count
