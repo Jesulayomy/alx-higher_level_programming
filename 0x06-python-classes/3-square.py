0d3bd595716b860f84f48dcdea8cfb787ab7dca8
Jesulayomy <jesulayomi081@gmail.com>
2023-01-24 17:53:12 -0800

    area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,15 +3,16 @@
 
 
 class Square:
-    """Square Doc"""
+    """Square Docstring"""
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initializes size as a private attribute"""
-        try:
-            size = int(size)
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = size
-        except TypeError("size must be an integer")
-            pass
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the area of the square object/instance"""
+        return (self.__size ** 2)
