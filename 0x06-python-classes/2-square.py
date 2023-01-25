e467a00aca761b9829a7c523f814a9d98170782f
Jesulayomy <jesulayomi081@gmail.com>
2023-01-24 17:45:37 -0800

    test

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,10 +7,10 @@ class Square:
 
     def __init__(self, size=0):
         """Initializes size as a private attribute"""
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                try:
-                    self.__size = size
-                except TypeError("size must be an integer"):
-                    pass
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            try:
+                self.__size = size
+            except TypeError("size must be an integer"):
+                pass
