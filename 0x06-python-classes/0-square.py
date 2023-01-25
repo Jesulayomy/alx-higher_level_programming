974cd40d12c9826b57a8989a6c03a923d88d680c
Jesulayomy <jesulayomi081@gmail.com>
2023-01-24 17:29:54 -0800

    docs

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+'''Creates a new class square'''
 class Square:
+    '''Square Class does nothing'''
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" Initializes with size"""
 class Square:
+    """Square Doc"""
     def __init__(self, size):
+        """Why do i have to doc init"""
         self.__size = size
