22d4c0574322a64294eddeb6f692839c210e6c97
Jesulayomy <jesulayomi081@gmail.com>
2023-02-07 07:46:08 -0800

    pascal

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Module returns a lisr opf list for the pascal's triangle of size n
+"""
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    start = [1, 1]
+    result = []
+    result += [[1]]
+    for i in range(1, n):
+        result += [start]
+        new = [1]
+        for idx in range(len(start) - 1):
+            new += [start[idx] + start[idx + 1]]
+        new += [1]
+        start = new
+    return result
