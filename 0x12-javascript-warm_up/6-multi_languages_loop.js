64f708c38585c1413926a2844407759718221212
Jesulayomy <jesulayomi081@gmail.com>
2023-04-12 04:53:04 +0000

    string fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const arrLines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const arrLines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const arrLine of arrLines) {
   console.log(arrLine);
 }
