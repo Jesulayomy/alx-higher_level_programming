6e4127dcdb8cdd189b15a589238999238e742b09
Jesulayomy <jesulayomi081@gmail.com>
2023-04-15 14:02:46 +0000

    import block

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,27 +5,28 @@
     hbtn_0e_0_usa with name starting with 'N'
 """
 
-import MySQLdb
-import sys
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
-hb = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user="{}".format(sys.argv[1]),
-        passwd="{}".format(sys.argv[2]),
-        db="{}".format(sys.argv[3]))
+    hb = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user="{}".format(sys.argv[1]),
+            passwd="{}".format(sys.argv[2]),
+            db="{}".format(sys.argv[3]))
 
-cur = hb.cursor()
-cur.execute("""
-        SELECT
-            id, name
-        FROM
-            states
-        WHERE
-            name LIKE 'N%'
-        ORDER BY
-            states.id
-        """)
-rows = cur.fetchall()
-for row in rows:
-    print(f"({row[0]}, '{row[1]}')")
+    cur = hb.cursor()
+    cur.execute("""
+            SELECT
+                id, name
+            FROM
+                states
+            WHERE
+                name LIKE 'N%'
+            ORDER BY
+                states.id
+            """)
+    rows = cur.fetchall()
+    for row in rows:
+        print(f"({row[0]}, '{row[1]}')")
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,28 +4,28 @@
     This module filters states from the database
     hbtn_0e_0_usa with name starting with user input argv4
 """
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
 
-import MySQLdb
-import sys
+    hb = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user="{}".format(sys.argv[1]),
+            passwd="{}".format(sys.argv[2]),
+            db="{}".format(sys.argv[3]))
 
-hb = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user="{}".format(sys.argv[1]),
-        passwd="{}".format(sys.argv[2]),
-        db="{}".format(sys.argv[3]))
-
-cur = hb.cursor()
-cur.execute("""
-    SELECT
-        id, name
-    FROM
-        states
-    WHERE
-        name = '{}'
-    ORDER BY
-        states.id
-    """.format(sys.argv[4]))
-rows = cur.fetchall()
-for row in rows:
-    print(f"({row[0]}, '{row[1]}')")
+    cur = hb.cursor()
+    cur.execute("""
+        SELECT
+            id, name
+        FROM
+            states
+        WHERE
+            name = '{}'
+        ORDER BY
+            states.id
+        """.format(sys.argv[4]))
+    rows = cur.fetchall()
+    for row in rows:
+        print(f"({row[0]}, '{row[1]}')")
