db9c7d8994eaf0d0ec2a6e80ccfc15fb62a6e312
Jesulayomy <jesulayomi081@gmail.com>
2023-04-17 00:38:28 +0000

    files advanced

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""
+    Creates the State “California” with the City
+    “San Francisco” from the database hbtn_0e_100_usa
+"""
+
+if __name__ == "__main__":
+    import sys
+    from relationship_state import Base, State
+    from relationship_city import City
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    driver = "mysql+mysqldb"
+    uname = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+    engine = create_engine(
+            '{}://{}:{}@localhost:3306/{}'.format(
+                driver,
+                uname,
+                password,
+                dbname),
+            pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name='California')
+    new_city = City(name='San Francisco')
+
+    new_state.cities.append(new_city)
+
+    session.add(new_state)
+    session.add(new_city)
+    session.commit()
