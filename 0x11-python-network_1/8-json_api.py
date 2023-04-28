50cfe41d7723a6f83bef33da6bd2e065ddaa3799
Jesulayomy <jesulayomi081@gmail.com>
2023-04-28 19:51:25 +0000

    Fix: Task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -6,21 +6,22 @@ if __name__ == '__main__':
     import requests
     import sys
 
+    q = ""
+
     try:
         q = sys.argv[1]
-    except IndexError:
-        q = ""
+    except:
+        pass
 
-    result = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
+    result = requests.post("http://0.0.0.0:5000/search_user", {'q': q})
 
     try:
         json_rep = result.json()
+        if not json_rep:
+            print("No result")
+        else:
+            print("[{}] {}".format(
+                json_rep.get('id'),
+                json_rep.get('name')))
     except ValueError:
         print("Not a valid JSON")
-    else:
-        if json_rep:
-            print("[{}] {}".format(
-                json_rep['id'],
-                json_rep['name']))
-        else:
-            print("No result")
