0f65225081dc6b17d8a62cbfa18e2a6842141338
Jesulayomy <jesulayomi081@gmail.com>
2023-04-28 17:23:54 +0000

    Files: Added files

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""
+    displays the value of X-Request-Id from the site passed in
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    result = requests.get(sys.argv[1])
+    print(result.headers["X-Request-Id"])
