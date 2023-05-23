befb0b4693412076168221270dfcdbc758ede9ea
Jesulayomy <jesulayomi081@gmail.com>
2023-05-23 18:37:39 +0000

    Files: first tasks

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const writeMe = require('fs');
+const content = process.argv[3];
+
+writeMe.writeFile(process.argv[2], 'utf-8', content, err => {
+  if (err) {
+    console.error(err);
+  }
+});
